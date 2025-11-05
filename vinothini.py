07ad2b9bdb4af0956a9f6a40a76da330cf81ab18
Vinoithini <vino636053812@gmail.com>
2025-11-05 16:07:42 +0530

    Add files via upload

--- vinothini.py
+++ vinothini.py
@@ -1,6 +1,24 @@
-name=input("my name")
-age=int (input("my age"))
-address=input ("my address")
-print("name:",name)
-print("age:",age)
-print("address:",address)
+print("Select an operation")
+print("1 Addition(+)")
+print("2 Subtraction(-)")
+print("3 Multiplication(*)")
+print("4 Division(/)")
+
+choice = input("Enter choice (1/2/3/4): ")
+
+num1 = float(input ("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if choice =='1':
+   print("Result =", num1 + num2)
+elif choice =='2':
+    print("Result =", num1 - num2)
+elif choice =='3':
+    print("Result =", num1 * num2)
+elif choice == '4':
+    if num2 != 0:
+        print("Result =", num1 / num2)
+    else:
+        print("Error: Cannot divide by zero!")
+else:
+        print("Invalid input! please choose 1, 2, 3, 4 ")
