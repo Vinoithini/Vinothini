45e3c269bae5cb84fecc5ec155d2c4420b7430c2
Vinoithini <vino636053812@gmail.com>
2025-11-08 11:59:14 +0530

    Add files via upload

---
+++ The best food if condition . py.py
@@ -0,0 +1,10 @@
+food = input("What's the best food?:")
+
+if food == "rice":
+    print("Yes,rice is the best.")
+
+elif food == "apple":
+     print("Apples aren't my cup of ruce.")
+
+else:
+    print("Never heard if it!")
